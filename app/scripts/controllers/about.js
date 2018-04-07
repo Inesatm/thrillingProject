80c202c92c66811a1dd7c301ef0dbaf127c7e8e4
Inesatm <ineskatam@gmail.com>
2018-04-07 18:28:10 +0200

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thrillingProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the thrillingProjectApp
+ */
+angular.module('thrillingProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thrillingProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thrillingProjectApp
+ */
+angular.module('thrillingProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
